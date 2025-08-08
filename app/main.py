482bbb7dcf79cf1ec9a4516d03149fc393f36da3
Hamlet Vasquez <hamlet.vasquez@bis.doc.gov>
2025-08-08 10:36:07 -0400

    Merging pyside6 mvc rewrite into main

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6.QtWidgets import QApplication
+
+from app.views.main_window import MainWindow
+from app.controllers.main_controller import MainController
+
+def main():
+    """
+    Initializes and runs the application.
+    """
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    controller = MainController(window)
+
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
